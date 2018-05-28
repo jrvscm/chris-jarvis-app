f728c5b43bbb1c9b79d53b066b091f6316f185bd
Chris Jarvis <jrvscm@gmail.com>
2018-05-27 18:02:12 -0600

    working on post structure

---
+++ src/components/Blog/FeaturedPosts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import glamorous from 'glamorous';
+
+import PostPreview from '../PostPreview';
+
+const FeaturedPosts = (posts) => (
+
+)
+
+const Container = glamorous.div({
+	display: `flex`,
+	flexDirection: `Column`,
+	justifyContent: `center`,
+	alignItems: `center`
+})
+
